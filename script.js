fb151204f26ce0d12ea410c57ad419f00f0ed74c
komalsingh64 <komalsingh124jsr@gmail.com>
2026-06-10 00:54:10 +0530

    Portfolio Website

---
+++ script.js
@@ -0,0 +1,19 @@
+body{
+    background-color: #111827;
+    color: white;
+    font-family: Arial, sans-serif;
+    text-align: center;
+    padding: 40px;
+}
+
+h1{
+    color: #60a5fa;
+}
+
+h2{
+    color: #fbbf24;
+}
+
+ul{
+    list-style: none;
+}
